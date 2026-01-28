4e521b306c566178d33a6c9095d555b61710b35d
drg25ii <93009091+drg25ii@users.noreply.github.com>
2026-01-28 20:26:24 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cd ../aiometadata && docker compose down && docker compose up -d --build
 cd ../easyproxy && docker compose down && docker compose up -d --build
 cd ../npm && docker compose down && docker compose up -d --build
 docker system prune -f
-echo "Deploy complet!"
+echo "Deploy complet Stremio!"
